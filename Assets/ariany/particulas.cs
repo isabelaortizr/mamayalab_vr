efaca85b08894bf0eb381f040fb367af01c97cff
Ariany Claure Cota <arianyclaure@gmail.com>
2025-10-26 15:25:29 -0400

    particulas
    toque de letrero

--- Assets/ariany/particulas.cs
+++ Assets/ariany/particulas.cs
@@ -1,31 +1,27 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class particulas : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
     public GameObject efectoParticulas;
 
-        private void OnTriggerEnter(Collider other)
-        {
+    private void OnTriggerEnter(Collider other)
+    {
         if (other.CompareTag("Player"))
         {
-            GameObject efecto = Instantiate(efectoParticulas, transform.position, transform.rotation);
-            Destroy(efecto, 2f);
-            Destroy(this.gameObject);
-        }
-    }
+            // La línea clave para solucionar el error CS0104:
+            UnityEngine.Debug.Log("¡TRIGGER DE PARTÍCULAS ACTIVADO!");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            GameObject efecto = Instantiate(efectoParticulas, transform.position, transform.rotation);
+
+            ParticleSystem ps = efecto.GetComponent<ParticleSystem>();
+
+            if (ps != null)
+            {
+                ps.Play();
+                Destroy(efecto, ps.main.duration);
+            }
+
+            this.enabled = false;
+        }
     }
 }
