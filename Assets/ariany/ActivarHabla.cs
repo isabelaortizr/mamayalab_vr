ea02712020fb109df4510d0fab1ce9f1252b61d5
ElMender <elmenderlel@outlook.com>
2025-11-01 14:16:58 -0400

    Codigo Actualizado

--- Assets/ariany/ActivarHabla.cs
+++ Assets/ariany/ActivarHabla.cs
@@ -3,25 +3,39 @@ using System.Collections;
 
 public class ActivarHabla : MonoBehaviour
 {
-    public Animator npcAnimator;
+    public AudioSource fuenteDeAudio1, fuenteDeAudio2 , fuenteDeAudio3, fuenteDeAudio4, fuenteDeAudio5, fuenteDeAudio6, fuenteDeAudio7, fuenteDeAudio8;
 
-   
-    private void OnTriggerEnter(Collider other)
+    public void playAudio1()
     {
-        if (other.CompareTag("Player"))
-        {
-            // Activa la animación de Hablar
-            npcAnimator.SetBool("IsTalking", true);
-
-            // Inicia una Corrutina para que el NPC hable por un tiempo fijo
-            StartCoroutine(TerminarDeHablar(5f));
-        }
+        fuenteDeAudio1.Play();
+    }
+    public void playAudio2()
+    {
+        fuenteDeAudio2.Play();
+    }
+    public void playAudio3()
+    {
+        fuenteDeAudio3.Play();
+    }
+    public void playAudio4()
+    {
+        fuenteDeAudio4.Play();
+    }
+    public void playAudio5()
+    {
+        fuenteDeAudio5.Play();
+    }
+    public void playAudio6()
+    {
+        fuenteDeAudio6.Play();
+    }
+    public void playAudio7()
+    {
+        fuenteDeAudio7.Play();
+    }
+    public void playAudio8()
+    {
+        fuenteDeAudio8.Play();
     }
 
-    IEnumerator TerminarDeHablar(float tiempo)
-    {
-        yield return new WaitForSeconds(tiempo);
-
-        npcAnimator.SetBool("IsTalking", false);
-    }
 }
